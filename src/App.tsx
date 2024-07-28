00bbfd85076dd12f5fb4655a04d2a9ec976f67ca
ace34TT <tafinasoa35@gmail.com>
2024-07-28 19:37:07 +0300

    setup : setup sign-up route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<SignUp />}></Route>
+      <Route path="/" element={<SignIn />}></Route>
+      <Route path="/sign-up" element={<SignUp />}></Route>
     </Routes>
   );
 }
